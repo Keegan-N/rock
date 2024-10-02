43de09d15bb12374f8b263147425bc5394444a29
keegan <letmeuseai66@gmail.com>
2024-10-02 12:33:48 -0700

    Initial Commit

---
+++ rock/rockApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockApp.swift
+//  rock
+//
+//  Created by Norgard, Keegan - Student on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct rockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
